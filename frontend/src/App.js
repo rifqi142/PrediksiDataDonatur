28f737b93ad0a88ca0de19b8ea8ba274e35cefa7
Muhammad Rifqi Setiawan <muhammadrifqi142@gmail.com>
2023-05-30 15:57:13 +0700

    chore: test implementation login pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
-import "./App.css";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Login } from "./Pages";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Doing job
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />}></Route>
+    </Routes>
   );
 }
-
-export default App;
---
+++ frontend/src/Pages/Login/login.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function login() {
+  return <h1>Login Here</h1>;
+}
