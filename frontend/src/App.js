697cefa6075cf090e1cbbf699da72ef02f4e0c7c
Muhammad Rifqi Setiawan <muhammadrifqi142@gmail.com>
2023-05-31 01:17:41 +0700

    chore: implementation register and login pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Login } from "./Pages";
+import { Login, Register } from "./Pages";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
     </Routes>
   );
 }
--- frontend/src/Pages/Register/register.jsx
+++ frontend/src/Pages/Register/register.jsx
@@ -8,23 +8,24 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 
-import "./login.css";
-import logillus from "../../Assets/login.png";
+import "./register.css";
+import regillus from "../../Assets/register.png";
 
 const handleSubmit = (e) => {};
 
-export default function login() {
+export default function register() {
   return (
-    <section id="login-pages">
+    <section id="register-pages">
       <Container>
         <Row>
           <Col md={6}>
             <Card>
               <Card.Body>
-                <Card.Title>Welcome back</Card.Title>
+                <Card.Title>Register Account</Card.Title>
                 <br />
                 <Card.Subtitle>
-                  Login to your account to use all feature
+                  Let's get you all set up so you can verify your personal
+                  account and begin setting up your profile.
                 </Card.Subtitle>
                 <hr />
                 <br />
@@ -33,23 +34,33 @@ export default function login() {
                     <Form.Label>Email address</Form.Label>
                     <Form.Control
                       type="email"
+                      id="email"
                       placeholder="Enter your email ..."
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                      type="text"
+                      id="username"
+                      placeholder="Enter your username ..."
                     />
                   </Form.Group>
                   <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control
-                      type="email"
+                      type="password"
+                      id="password"
                       placeholder="Enter your password ..."
                     />
                   </Form.Group>
                   <Button variant="primary" type="submit" className="btn-login">
-                    Login
+                    Register
                   </Button>
                   <div className="make-account mt-4">
-                    <span>Doesn't have an account ? </span>
-                    <a href="/register" className="text-decoration-none">
-                      SignUp Here
+                    <span>Already have an account ? </span>
+                    <a href="/" className="text-decoration-none">
+                      Login Here
                     </a>
                   </div>
                 </Form>
@@ -57,8 +68,8 @@ export default function login() {
             </Card>
           </Col>
           <Col md={6}>
-            <div className="illustration-login">
-              <img src={logillus} alt="logo" className="img-fluid" />
+            <div className="illustration-register">
+              <img src={regillus} alt="logo" className="img-fluid" />
             </div>
           </Col>
         </Row>
