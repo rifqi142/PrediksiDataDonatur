9e80f1d3507219672e839d549e53d220bd0232b0
Muhammad Rifqi Setiawan <muhammadrifqi142@gmail.com>
2023-06-16 14:44:00 +0700

    update migration table datase and hasil_prediksi

--- backend/app/model/dataset.py
+++ backend/app/model/dataset.py
@@ -3,10 +3,12 @@ from app import db
 class Dataset(db.Model):
     id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
     id_master = db.Column(db.BigInteger, db.ForeignKey('master.id'), nullable=False)
-    no = db.Column(db.String(255))
-    tanggal = db.Column(db.DateTime)
+    tahun = db.Column(db.BigInteger)
+    bulan = db.Column(db.BigInteger)
     jenis_donasi = db.Column(db.String(255))
     jumlah_donasi = db.Column(db.BigInteger)
+    jumlah_data = db.Column(db.BigInteger)
+    
     
     def __repr__(self):
         return '<Dataset {}>'.format(self.name)
--- backend/app/model/hasil_prediksi.py
+++ backend/app/model/hasil_prediksi.py
@@ -2,7 +2,10 @@ from app import db
 
 class Hasil_prediksi(db.Model):
     id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-    tanggal = db.Column(db.DateTime, nullable=False)
+    id_master = db.Column(db.BigInteger, db.ForeignKey('master.id'), nullable=False)
+    tahun = db.Column(db.BigInteger)
+    bulan = db.Column(db.BigInteger)
+    jenis_donasi = db.Column(db.String(255))
     prediksi_donasi = db.Column(db.BigInteger, nullable=False)
     ekspektasi_donasi = db.Column(db.BigInteger, nullable=False)
     mape = db.Column(db.BigInteger, nullable=False)
